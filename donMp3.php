fdc543b971195820da7e9e5ec73d23d8e56749a2
Moises <zaer00t@gmail.com>
2015-06-10 12:17:54 -0500

    Descripcion de funcionamiento
    Se agrega un segmento de comentarios describiendo como se utiliza este script.

--- donMp3.php
+++ donMp3.php
@@ -1,4 +1,16 @@
 #!/usr/bin/php
+/**********************************************************/
+/* El archivo que se le pasa como parametro al script es  */
+/* una lista de links que hacen referencia a los archivos */
+/* a descargar, por ejemplo:                              */
+/* <a href="mi_mp3.mp3">Cancion X</a>                     */
+/* ¿Como obtenemos la lista?                              */
+/* Con la ayuda de Google Dorks, ejemplo:                 */
+/* intitle:index.of? mp3 Un millon de primaveras          */
+/* obtienen la lista de los archivos, guardamos y pasamos */
+/* como parametro esa lista y listo                       */
+/* dudas: zaer00t@gmail.com                               */
+/**********************************************************/
 <?php
 function extrae_ligas($archivo)
 {
@@ -45,7 +57,7 @@ $usage ="\n\t##############################################\n" .
 		"\t# El modo de uso para {$argv[0]} es:        #\n".
 		"\t#  {$argv[0]} <lista>                       #\n".
 		"\t#  Donde lista puede ser cualquier tipo de   #\n".
-		"\t#  de archivo que contenga la lista de links #\n".
+		"\t#  archivo que contenga la lista de links    #\n".
 		"\t##############################################\n\n\n";
 // the start the magic code
 if(!isset($argv[1]))
